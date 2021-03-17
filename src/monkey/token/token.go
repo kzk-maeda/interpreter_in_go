374d6585425a2c6002f8ebe456c727ba058e5f0c
kzk-maeda <kzk.maeda0711@gmail.com>
2021-03-17 21:25:39 +0900

    expand lexer

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -16,12 +16,12 @@ const (
 	INT   = "INT"
 
 	// 演算子
-	ASSIGN = "="
-	PLUS   = "+"
-	MINUS  = "-"
-	BANG   = "!"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
 	ASTERISK = "*"
-	SLASH  = "/"
+	SLASH    = "/"
 
 	LT = "<"
 	GT = ">"
@@ -38,11 +38,21 @@ const (
 	// Keyword
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
-var keywords = map[string]TokenType {
-	"fn": FUNCTION,
-	"let": LET,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // LookupIdent 関数
